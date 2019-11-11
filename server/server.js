f779ee471db6e0f22e422b377854c345917d01b4
andirsun <ander.laverde.dev@gmail.com>
2019-11-11 02:33:15 -0500

    install cors and file upload modules

--- server/server.js
+++ server/server.js
@@ -3,10 +3,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const sqlite3 = require('sqlite3').verbose()
-
+const sqlite3 = require('sqlite3').verbose();
+const fileUpload = require('express-fileupload');
+const cors = require('cors')
 
 //////////////////////////////////////////
+app.use(cors());//allow petitions
+// Using module express-fileupload to upload files to server
+app.use(fileUpload({ useTempFiles: true })); 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
  
@@ -37,7 +41,10 @@ mongoose.connect('mongodb://localhost:27017/resturant', (err)=>{
   console.log(`Base de datos mela`);
 });
 
-
+process.on('uncaughtException', function (error) {
+    console.log(error.stack);
+    
+ });
 app.listen(process.env.PORT,()=>{
     console.log('escuchando al puerto',3000);
 });
